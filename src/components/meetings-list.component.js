baba48d5e6f9c396cc83d5d9c04dd1d1406355d5
Sarah <sarahcheah22@gmail.com>
2020-12-21 00:49:14 +0800

    comment our collapsible JS code

--- src/components/meetings-list.component.js
+++ src/components/meetings-list.component.js
@@ -58,18 +58,18 @@ export default class MeetingsList extends Component {
   }
 
   render() {
-    var collapse = document.getElementsByClassName("collapsible");
-    for (let i = 0; i < collapse.length; i++) {
-        collapse[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var content = this.nextElementSibling;
-        if (content.style.display === "block") {
-          content.style.display = "none";
-        } else {
-          content.style.display = "block";
-        }
-      });
-    }
+    // var collapse = document.getElementsByClassName("collapsible");
+    // for (let i = 0; i < collapse.length; i++) {
+    //     collapse[i].addEventListener("click", function() {
+    //     this.classList.toggle("active");
+    //     var content = this.nextElementSibling;
+    //     if (content.style.display === "block") {
+    //       content.style.display = "none";
+    //     } else {
+    //       content.style.display = "block";
+    //     }
+    //   });
+    // }
     return (
       <div>
         <h3>Logged Meetings</h3>
